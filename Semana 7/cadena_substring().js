6b3107ca6fa5666963177b55b3cb674cbddfac16
Ben4010 <117422467+Ben4010@users.noreply.github.com>
2023-01-09 19:17:44 -0600

    Create cadena_substring().js

---
+++ Semana 7/cadena_substring().js
@@ -0,0 +1,9 @@
+ function primera_palabra(a) {
+ return work.substring (0, work.indexOf(" "));
+
+ }
+
+ let work = "see and stop";
+ console.log(primera_palabra(work));
+
+ //esta funcion da como resultado la string see que se solicitaba en el ejercicio
