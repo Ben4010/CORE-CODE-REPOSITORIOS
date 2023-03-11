c595ab53cf2fe2cf94a958fe9b721dce7301f252
Benneth <117422467+Ben4010@users.noreply.github.com>
2023-03-11 16:24:22 -0600

    Create Questions.js

---
+++ Weeks/Week 12/Solutions/Questions.js
@@ -0,0 +1,9 @@
+¿Qué es Node.JS?
+¿Qué es NPM?
+¿Qué problema resuelve Node.JS? (¿Hay algún problema que se pueda resolver con Node.JS?🤔)?
+¿Qué es el motor Javascript V8?
+¿Es realmente necesario Node.JS en el ecosistema de Desarrollo?
+¿Por qué no usar PHP o Golang?
+¿Cuál es la diferencia entre Node.JS y cualquier otro navegador?
+¿Son lo mismo Node.JS y un navegador?
+¿Qué es NVM y por qué es útil para los desarrolladores de Node.JS?
